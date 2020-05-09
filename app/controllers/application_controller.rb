ab13144ebd0c3cd49b3c59a1f3663f7fdfc11d80
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-05-09 03:41:43 +0200

    create product views & routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action:render_categories
+    before_action :render_categories,:render_products
     def render_categories
         @categories=Category.all
     end
     
+    def render_products
+        @products = Product.all
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/index'
   resources :categories
   resources :brands
+  resources :products
  
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
