fabfa8bd7026dad55721dc046dd97470667e23a5
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-05-10 23:24:58 +0200

    fix product delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_051601) do
+ActiveRecord::Schema.define(version: 2020_05_10_211337) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -56,17 +56,17 @@ ActiveRecord::Schema.define(version: 2020_05_10_051601) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "create_orders_products", id: false, force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "product_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "state"
+  end
+
+  create_table "orders_products", id: false, force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "product_id"
+    t.index ["order_id"], name: "index_orders_products_on_order_id"
+    t.index ["product_id"], name: "index_orders_products_on_product_id"
   end
 
   create_table "product_imgs", force: :cascade do |t|
