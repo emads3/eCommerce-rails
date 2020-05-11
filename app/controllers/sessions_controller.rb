c302a243563a5bd6fbc3cdcc04dba7e7d8f4be1a
khaldmansour <khaldmansour93@gmail.com>
2020-05-11 10:52:09 +0200

    resolving conflicts

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+    # def new
+    #     if session[:user]
+    #       @user = User.find(session[:user])
+    #     end
+    # end
+
+    # def create
+    #     # ...
+    #     session[:user_id] = @user.id
+    #     # ...
+    #   end
+   
+end
