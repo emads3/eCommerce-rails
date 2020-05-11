bc350f082be839214bb56e539347ba10460ee36f
nouranebrahim <nouranebrahim9@gmail.com>
2020-05-11 07:56:22 +0200

    the user can show the shopping_cart of him only , delete product based on the stock_quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_230321) do
+ActiveRecord::Schema.define(version: 2020_05_11_034333) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_230321) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_seller"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
