baaf3cc4ff9ecd1c93adeaf4c90f2132f0034592
Fatma Mohammed <gfatmamohammed@gmail.com>
2020-05-12 04:24:29 +0200

    fix sign out bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   get 'home/index'
   get 'shopping_carts/show' , to:'shopping_carts#show'
   get 'shopping_carts/:product' , to:'shopping_carts#index'
