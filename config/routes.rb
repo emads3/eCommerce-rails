1444256d986fa42a9efc4d8ae8006ea5c702b0dd
Emad Saeed <e@emads3.com>
2020-05-12 06:38:49 +0200

    create orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :brands
   resources :products
+  resources :orders
  
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
