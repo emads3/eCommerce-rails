19f02b1f0be73a42e9ccba83208e0073173a43e3
Emad Saeed <e@emads3.com>
2020-05-13 05:22:52 +0200

    add more seeds for easier/faster development

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'nouran@yahoo.com', password: '987*654*321', password_confirmation: '987*654*321') if Rails.env.development?
 AdminUser.create!(email: 'e@emads3.com', password: '123123123', password_confirmation: '123123123') if Rails.env.development?
+
+# User.create(email: 'emad.fcis@gmail.com', encrypted_password: '123123123', created_at: '2020-05-13 03:45:15.000', updated_at: '2020-05-13 03:45:15.000')
+
+Brand.create(name: 'Adidas') if Rails.env.development?
+Brand.create(name: 'Samsung') if Rails.env.development?
+
+Category.create(name: 'Electrical Devices')
+Category.create(name: 'Mobile Devices')
+Category.create(name: 'Computers And Accessories')
+
+# Store.create
