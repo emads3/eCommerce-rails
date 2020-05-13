fae4de63df775e387df26ddce51e29a502081e4e
Emad Saeed <e@emads3.com>
2020-05-13 05:31:01 +0200

    merge two migrations

--- db/migrate/20200507151644_create_orders.rb
+++ db/migrate/20200507151644_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.string :state
+      t.boolean :is_checked, { null: false, default: false }
 
       t.timestamps
     end
--- db/migrate/20200511115900_add_is_checked_to_orders.rb
+++
@@ -1,6 +0,0 @@
-class AddIsCheckedToOrders < ActiveRecord::Migration[6.0]
-  def change
-    add_column :orders, :is_checked, :boolean,
-               null: false, default: false
-  end
-end
