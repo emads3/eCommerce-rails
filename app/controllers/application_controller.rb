64a630ce5400255d01c5d63e3e3377d45796eecc
Emad Saeed <e@emads3.com>
2020-05-14 18:39:31 +0200

    fix homepage rendering bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    
+
     before_action :render_categories,:render_products,:render_brands,:render_orders,:render_stores
     def render_categories
         @allCategories=Category.all
     end
-    
+
     def render_products
-        @products = Product.order(:name).page(params[:page]).per(10)
+        @products = Product.order(:title).page(params[:page]).per(10)
     end
 
     def render_brands
@@ -20,5 +20,5 @@ class ApplicationController < ActionController::Base
     def render_stores
         @allstores=Store.all
     end
-    
+
 end
