cca321c2cf60b7be19bd21acbbe1637953cf0d30
Emad Saeed <e@emads3.com>
2020-05-15 12:05:51 +0200

    add billing_address & shipping_address to orders table

--- db/migrate/20200507151644_create_orders.rb
+++ db/migrate/20200507151644_create_orders.rb
@@ -2,6 +2,8 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.boolean :is_checked, { null: false, default: false }
+      t.string :billing_address
+      t.string :shipping_address
 
       t.timestamps
     end
