b0fde00ffbb35cf04d2ec0e254cc492ca3c0271d
Emad Saeed <e@emads3.com>
2020-05-16 15:15:40 +0200

    add quantity ckeck in cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,25 +15,41 @@ class CartController < ApplicationController
   end
 
   def create
-    @product_id = params[:product_id].to_i
-    redirect_to(home_index_path, notice: 'product not found') unless @product_id
+    product_id = params[:product_id].to_i
+    redirect_to(home_index_path, notice: 'product not found') unless product_id
 
     # get the shopping cart of the user
-    @cart = Order.where(user_id: current_user.id, is_checked: false).first
-    unless @cart # there is no cart, create one
-      @cart = Order.new user_id: current_user.id, is_checked: false
-      @cart.save
+    cart = Order.where(user_id: current_user.id, is_checked: false).first
+    unless cart # there is no cart, create one
+      cart = Order.new user_id: current_user.id, is_checked: false
+      cart.save
     end
 
     # check if the product already in the cart,
     # get it ..
-    @prod = @cart.OrderProducts.find_by product_id: @product_id
-    if !@prod
-      @cart.OrderProducts.create product_id: @product_id, quantity: 1
+    cart_item = cart.OrderProducts.find_by product_id: product_id
+    # stock_quantity = cart_item.product.stock_quantity if cart_item != nil
+    stock_quantity = Product.find(product_id).stock_quantity
+    notice = ''
+
+    if cart_item # item already in the cart, only update quantity
+      if stock_quantity.zero?
+        cart_item.update(quantity: 0)
+        notice = 'not enough quantity in available stock'
+      elsif stock_quantity <= cart_item.quantity
+        cart_item.update(quantity: stock_quantity)
+        notice = 'You got all the available stock'
+      else
+        cart_item.update(quantity: cart_item.quantity + 1)
+        notice = 'successfully added item to cart'
+      end
+    elsif stock_quantity.positive? # stock_quantity > 0 # not in the cart, add it
+      cart.OrderProducts.create product_id: product_id, quantity: 1
+      notice = 'successfully added item to cart'
     else
-      @prod.update(quantity: @prod.quantity + 1)
+      notice = 'Not enough quantity stock'
     end
-    redirect_to cart_path, notice: 'successfully added item to cart' # TODO : localize it
+    redirect_to cart_path, notice: notice # TODO : localize it
   end
 
   def destroy
